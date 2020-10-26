93a23577b51107cfb98ae314364006c1c56c2594
Edgar <mmnledgar@gmail.com>
2020-10-26 09:09:38 +0000

    module_home_view_caissiere

--- resources/views/caissiere/pages/home.blade.php
+++ resources/views/caissiere/pages/home.blade.php
@@ -1,6 +1,11 @@
 @extends("caissiere.layout.layout_caissiere")
 
+@section("css")
+    <link rel="stylesheet" href="{{URL::asset("css/css_caissiere/home.css")}}" />
+@endsection
 
 @section("content")
     <h1>HOME CAISSIERE</h1>
+
+<img src="{{URL::asset('avatarFemme.png')}}" id="imageIcon" alt="icone femme" />
 @endsection
