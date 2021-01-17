bdae1b17dd9b00e2be2c2239a08bc465fb0b3cc8
Edgar <mmnledgar@gmail.com>
2021-01-17 15:09:25 +0000

    finish module depot

--- app/Http/Controllers/caissiereController.php
+++ app/Http/Controllers/caissiereController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\comptes ;
 use App\compte_bloque;
 use App\compte_epargne;
+use App\compte_courant;
 
 use App\Http\Controllers\notification;
 
@@ -15,13 +16,15 @@ class caissiereController extends Controller
     private $_comptes;
     private $_compteBloque;
     private $_compteEpargne;
+    private $_compteCourant;
 
     public function __construct(comptes $comptes,compte_bloque $compteBloque
-    ,compte_epargne $compteEpargne)
+    ,compte_epargne $compteEpargne,compte_courant $compteCourant)
     {
         $this->_comptes = $comptes;
         $this->_compteBloque =$compteBloque;
         $this->_compteEpargne = $compteEpargne;
+        $this->_compteCourant = $compteCourant;
     }
 
     public function updateCompteEpargne($montant , $idCompte ,$numeroCompte){
@@ -48,6 +51,32 @@ class caissiereController extends Controller
 
     }
 
+
+    public function updateCompteCourant($montant , $idCompte ,$numeroCompte){
+
+        //we update the account 
+        $val =  $this->_compteCourant->updateSoldeCompteCourant($montant,$idCompte);
+
+        if($val==null){
+             //we prepare a notification 
+        notification::displayMessage("error"," DEPOT DE <<".$montant." FCFA >> SUR LE NUMERO
+        DE COMPTE <<".$numeroCompte.">> \n 
+       ERREUR DURANT LA TRANSACTION ");
+
+        }else{
+
+            //we prepare a notification 
+        notification::displayMessage("success"," DEPOT DE <<".$montant." FCFA >> SUR LE NUMERO
+        DE COMPTE <<".$numeroCompte.">> \n 
+       EFFECTUE AVEC SUCCES ");
+        }
+
+        //we return the route to redirect to the page 
+        return $route="/depot.html";
+
+   }
+
+
     public function insertDepot(Request $request){
         //get the variable form the form
         $montant = $request->get("montant");
@@ -66,7 +95,7 @@ class caissiereController extends Controller
             //make a redirection
             return redirect("/depot.html");
         }else{
-            //get the id of the account 
+            //get the id of the account
             $idCompte = $data->idCompte;
 
             //get the type of account => you should take a look on the database
@@ -85,14 +114,14 @@ class caissiereController extends Controller
                      DE COMPTE <<".$numeroCompte.">> \n 
                     EFFECTUE AVEC SUCCES ");
 
-                    //we redirect to the page 
+                    //we redirect to the page
                     $route="/depot.html";
                 break;
-                case "CE": 
+                case "CE":
                     $route = $this->updateCompteEpargne($montant,$idCompte,$numeroCompte);
                 break;
-                case "CC" :
-                    
+                case "CC":
+                    $route = $this->updateCompteCourant($montant , $idCompte ,$numeroCompte);
                 break;
             }
 
--- app/compte_courant.php
+++ app/compte_courant.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class compte_courant extends Model
 {
-    //
+    public  function updateSoldeCompteCourant(int $montant , int $idCompte){
+        //initialize the variable 
+        $compteCourant = null;
+
+        try {
+            $compteCourant = DB::update("UPDATE compte_courant
+             set solde = solde + ? where id_compte = ?",[$montant , $idCompte ]);
+        } catch (\Throwable $th) {
+            $compteCourant = null;
+        }
+
+        return $compteCourant;
+    } 
 }
